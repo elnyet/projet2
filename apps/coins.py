2e8f68ba4f652d5a21dab2358b78e5d550f7caf6
lenyet <153497028+elnyet@users.noreply.github.com>
2024-03-14 14:33:10 +0100

    Update coins.py

--- apps/coins.py
+++ apps/coins.py
@@ -1,12 +1,9 @@
-# rendue de monnet 
 def rendre_somme(argent):
     money=[100,50,20,10,5,2,1]
-    rent=[]
+    result=[]
     for i in money:
         if argent>=i:
             avg=argent//i
             argent-=avg*i
-            rent.append((i,avg))
-    return rent
-
-#
+            result.append((i,avg))
+    return result

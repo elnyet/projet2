9c55ba45f10aa9681c6a931523a2d984b4be48e1
lenyet <153497028+elnyet@users.noreply.github.com>
2024-03-14 14:56:16 +0100

    Delete apps/coins.py

--- apps/coins.py
+++
@@ -1,9 +0,0 @@
-def rendre_somme(argent):
-    money=[100,50,20,10,5,2,1]
-    result=[]
-    for i in money:
-        if argent>=i:
-            avg=argent//i
-            argent-=avg*i
-            result.append((i,avg))
-    return result

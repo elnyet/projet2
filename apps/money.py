9e5315c33301a60c5b628abc2489cf4ba9c43ffb
lenyet <153497028+elnyet@users.noreply.github.com>
2024-03-22 01:40:58 +1300

    Update money.py

--- apps/money.py
+++ apps/money.py
@@ -1,4 +1,5 @@
 def rendre_somme(argent):
+    # processus du rendu d'argent, avec une list contenant les billets utilisables et une liste de liste contenant les rendus
     money=[100,50,20,10,5,2,1]
     result=[]
     for i in money:
